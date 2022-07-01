7831111da4627c20eca36e0e2b6f42859ecb1f19
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-07-01 13:52:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482','6287877173955','0']
-global.ownerName = 'Fandyyy X All Creator'
-global.packname = 'Ichigo Kurosaki'
-global.author = 'Whatsapp Bot 2022'
+global.ownerNumber = ['6285730794089','6285730794089','0']
+global.ownerName = 'GilzzOFC'
+global.packname = 'GilzzMultiDevice'
+global.author = 'Botz Gilzz'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
 
